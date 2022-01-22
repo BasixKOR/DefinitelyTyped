d9dfc5ba529913489d413bfb2e5af231e54afbef
Basix <basix@basix.tech>
2022-01-22 13:44:58 +0900

    Add refiner-js

---
+++ types/refiner-js/index.d.ts
@@ -0,0 +1,25 @@
+type IdOrEmail = { id: string } | { email: string };
+
+interface Methods {
+    setProject(id: string): void;
+    identifyUser(meta: IdOrEmail & Record<string, any>): void;
+    resetUser(): void;
+    trackEvent(event: string): void;
+    showForm(surveyId: string, force?: boolean): void;
+    dismissForm(surveyId: string): void;
+    closeForm(surveyId: string): void;
+    addToResponse(meta: any): void;
+    stopPinging(): void;
+}
+
+interface Callbacks {
+    onShow(surveyId: string): void;
+    onDismiss(surveyId: string): void;
+    onClose(surveyId: string): void;
+    onComplete(surveyId: string, data: any): void;
+}
+
+declare function _refiner<T extends keyof Methods>(method: T, ...args: Parameters<Methods[T]>): ReturnType<Methods[T]>;
+declare function _refiner<T extends keyof Callbacks>(callback: T, fn: Callbacks[T]): void;
+
+export = _refiner;
